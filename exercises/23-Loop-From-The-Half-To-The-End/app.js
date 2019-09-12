6ce2d64c95ebfb8fb7f03c20c8fe7d0077652eca
Julio Londono <juliolondonogarcia@gmail.com>
2019-09-12 20:10:24 +0000

    2.3 Loop from the half to the end

---
+++ exercises/23-Loop-From-The-Half-To-The-End/app.js
@@ -0,0 +1,10 @@
+var mySampleArray = [3423,5,4,47889,654,8,867543,23,48,56432,55,23,25,12];
+
+var initialValue = 0;
+var stopValue = 0;
+var increasingValue = 0;
+
+for(var i = (mySampleArray.length/2); i<mySampleArray.length; i = i + 1)
+{
+	console.log(mySampleArray[i]);
+}
