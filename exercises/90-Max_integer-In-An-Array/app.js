f33a21454531c489ae2848ebfb52bc201f28cf84
Julio Londono <juliolondonogarcia@gmail.com>
2019-09-16 20:10:17 +0000

    10.1 forEach Min Val

--- exercises/90-Max_integer-In-An-Array/app.js
+++ exercises/90-Max_integer-In-An-Array/app.js
@@ -1,12 +1,1 @@
 var myArray = [43,23,6,87,43,1,4,6,3,67,8,3445,3,7,5435,63,346,3,456,734,6,34];
-
-  let maxValue =0;
-  
-  myArray.forEach((i)=> {
-    
-    if(maxValue<i) {
-      maxValue=i
-    }
-  });
-  
-  console.log(maxValue);
